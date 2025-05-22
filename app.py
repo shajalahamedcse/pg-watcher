9eabc5fa55da41272c5ccbf116b820b643731044
shajalahamedcse <shajalahamedcse@gmail.com>
2025-05-22 20:04:08 +0700

    hello

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/api/hello')
+def hello():
+    return jsonify({"message": "Hello, World!"})
+
+if __name__ == '__main__':
+    app.run(debug=True) 
